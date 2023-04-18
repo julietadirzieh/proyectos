10f5293016c237e3532ea4b223400a0bf3df7428
julietadirzieh <julidirzieh@gmail.com>
2023-04-18 16:37:59 -0300

    deleted translations

--- next-i18next.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    i18n: {
-        defaultLocale: 'en',
-        locales: ['en', 'es'],
-    },
-    fallbackLng: 'en', // Modifica el fallbackLng para que sea una cadena de texto en lugar de un objeto
-};
